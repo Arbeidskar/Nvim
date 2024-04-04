4aa706fc7f2988af1f47c6674d27101b4ab904b8
Arbeidskar <kristianskjell@hotmail.com>
2024-04-04 14:54:05 +0200

    First commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {"nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = { "c", "python", "sql", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+            sync_install = false, 
+            highlight = { enable = true },
+            indent = { enable = true },  
+        })
+    end
+}
